bbd93dfe123d601ceedbfcde2c871d82ba7b988b
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-04-07 20:50:56 +0700

    nho tutor

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,8 +7,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import StaffDetail from './StaffDetailComponent';
 import DepartmentList from './DepartmentListComponent';
 import Payroll from './PayrollComponent';
-import { Input, FormGroup } from 'reactstrap';
-import $ from 'jquery';
+import { Input, FormGroup, Form, Button, Col, Row, Modal, ModalHeader, ModalBody, Label, FormFeedback } from 'reactstrap';
 
 class Main extends Component {
 
@@ -22,7 +21,22 @@ class Main extends Component {
         selectedStaff: null,
         selectedDepartment: "all",
         searchInput: "",
+        isModalOpen: false,
+        name: '',
+        doB: '',
+        telnum: '',
+        email: '',
+        agree: false,
+        contactType: 'Tel.',
+        message: '',
+        detailStaff: {
+
+        }
     }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   onDepartmentChange(id) {
@@ -33,24 +47,44 @@ class Main extends Component {
     this.setState({searchInput: keyword});
   }
 
+  handleSearch(event) {
+    event.preventDefault();
+    let keyword = event.target.search.value;
+    this.onSearchInputChange(keyword);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleInputChange(event) {
+    const value = event.target.value
+    const name = event.target.name;
+    this.setState({
+      [name]:value
+    })
+}
+
+handleSubmit(event) {
+    console.log('Current State is: ' + JSON.stringify(this.state));
+    alert('Current State is: ' + JSON.stringify(this.state));
+    event.preventDefault();
+}
+
     render() {
 
       const StaffWithId = ({match}) => {
         return (
           <StaffDetail staff={this.state.staffs.filter((staff) => staff.id === parseInt(match.params.staffId,10))[0]} />
         );
-      }
+      };
 
       const ListStaff = () => {
         let staffs = this.state.staffs;
-        if (this.state.selectedDepartment === "all" && this.state.searchInput != "") {
-          staffs = staffs.filter((staff) => staff.name.search(this.state.searchInput) != -1);
-        }
-        if (this.state.selectedDepartment != "all") {
-          staffs = staffs.filter((staff) => staff.department.id === this.state.selectedDepartment);
-          if (this.state.searchInput != "") {
-            staffs = staffs.filter((staff) => staff.name.search(this.state.searchInput) != -1);
-          }
+        if (this.state.searchInput != "") {
+          staffs = staffs.filter((staff) => staff.name.toLowerCase().search(this.state.searchInput.toLowerCase()) != -1);
         }
         if (staffs.length > 0) {
           return (
@@ -64,37 +98,111 @@ class Main extends Component {
             </div>
           );
         }
-      }
+      };
 
-      const department = this.state.departments.map((department) => {
+      const StaffPage = () => {
         return (
-            <option value={department.id}>
-                {department.name}
-            </option>
+          <div className="container-fluid pl-5 pr-5">
+            <div className="row mt-2">
+                <h2 className="col-3 col-md-3 col-lg-3">Nhân viên</h2>
+                <div className='col-3 col-md-3 col-lg-3'>
+                  <button className="btn btn-secondary" onClick={this.toggleModal}> <i className='fa fa-plus'></i> </button>
+                </div>
+                <Form className="col-6 col-md-6 col-lg-6" onSubmit={this.handleSearch}>
+                  <Row className="form-group">
+                    <Col md={9}>
+                      <Input type="text" id="search" name="search" />
+                    </Col>
+                    <Col md={3}>
+                      <Button type="submit" color="primary">Tìm</Button>
+                    </Col>
+                  </Row>
+                </Form>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                  <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
+                  <ModalBody>
+                    <Form onSubmit={this.handleSubmit}>
+                      <FormGroup row>
+                          <Label htmlFor="name" md={2}>Tên</Label>
+                          <Col md={10}>
+                              <Input type="text" id="name" name="name"
+                                  value={this.state.name} 
+                                  onChange={this.handleInputChange} />
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Label htmlFor="doB" md={2}>Ngày sinh</Label>
+                          <Col md={10}>
+                              <Input type="date" id="doB" name="doB"
+                                  value={this.state.doB} 
+                                  onChange={this.handleInputChange} />
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
+                          <Col md={10}>
+                              <Input type="tel" id="telnum" name="telnum" placeholder="Contact Tel."
+                                  value={this.state.telnum} 
+                                  onChange={this.handleInputChange} />
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Label htmlFor="email" md={2}>Email</Label>
+                          <Col md={10}>
+                              <Input type="email" id="email" name="email" placeholder="Email"
+                                  value={this.state.email} 
+                                  onChange={this.handleInputChange} />
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Col md={{size: 6, offset: 2}}>
+                              <FormGroup check>
+                                  <Label check>
+                                      <Input type="checkbox"
+                                          name="agree"
+                                          checked={this.state.agree} 
+                                          onChange={this.handleInputChange} /> {' '}
+                                      <strong>May we contact you?</strong>
+                                  </Label>
+                              </FormGroup>
+                          </Col>
+                          <Col md={{size: 3, offset: 1}}>
+                              <Input type="select" name="contactType"
+                                      value={this.state.contactType} 
+                                      onChange={this.handleInputChange} >
+                                  <option>Tel.</option>
+                                  <option>Email</option>
+                              </Input>
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Label htmlFor="message" md={2}>Your Feedback</Label>
+                          <Col md={10}>
+                              <Input type="textarea" id="message" name="message" rows="12"
+                                  value={this.state.message} 
+                                  onChange={this.handleInputChange} />
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Col md={{size:10,offset:2}}>
+                              <Button type="submit" color="primary">Send Feedback</Button>
+                          </Col>
+                      </FormGroup>
+                    </Form>
+                  </ModalBody>
+                </Modal>
+            </div>
+            <hr/>
+            <ListStaff />
+          </div>
         );
-    })
+      }
 
       return (
       <div className='App'>
           <Header />
           <Switch>
-            <Route exact path="/staff" >
-              <div className="container-fluid pl-5 pr-5">
-                <div className="row mt-2">
-                    <h2 className="col-4 col-md-4 col-lg-4">Nhân viên</h2>
-                    <FormGroup className="col-2 col-md-2 col-lg-2">
-                        <Input type="select" id="department" onChange={() => this.onDepartmentChange($('#department').val())}>
-                            <option value="all" selected>All</option>
-                            {department}
-                        </Input>
-                    </FormGroup>
-                    <FormGroup className="col-6 col-md-6 col-lg-6">
-                        <Input id="search-staff" placeholder="Search..." onChange={() => this.onSearchInputChange($('#search-staff').val())} />
-                    </FormGroup>
-                </div>
-                <hr/>
-                <ListStaff />
-              </div>
+            <Route exact path="/staff" component={StaffPage}>
             </Route>
             <Route path="/staff/:staffId" component={StaffWithId} />
             <Route exact path="/department" component={() => <DepartmentList departments={this.state.departments} />} />
