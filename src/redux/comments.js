f3b40599948e012d6f128589ee0f1799f0e85efa
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-04-13 18:14:41 +0700

    Combining Reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,8 @@
+import {COMMENTS} from '../shared/comments';
+
+export const Commnents = (state = COMMENTS, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reduce';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes'; 
+import { Commnents } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders'; 
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers ({
+            dishes: Dishes,
+            comments: Commnents,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
     
     return store;
